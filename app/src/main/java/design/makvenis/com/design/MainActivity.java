7530039c6f4e86ad8f9f3ebe91b6f4a690130b92
makvenis <makvenis@163.com>
2018-12-03 10:27:32 +0800

    测试

--- app/src/main/java/design/makvenis/com/design/MainActivity.java
+++ app/src/main/java/design/makvenis/com/design/MainActivity.java
@@ -2,12 +2,24 @@ package design.makvenis.com.design;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.design.library.DesignTools;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = ((TextView) findViewById(R.id.mTest));
+
+        DesignTools designTools=new DesignTools();
+        String show = designTools.show("Library调用");
+        mTextView.setText(show);
     }
 }
---
+++ library/src/main/java/com/design/library/DesignTools.java
@@ -0,0 +1,10 @@
+package com.design.library;
+
+
+public class DesignTools {
+
+
+    public String show(String s){
+        return "返回值"+s;
+    }
+}
